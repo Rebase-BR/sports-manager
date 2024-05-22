6029c54d769fd686765e6037b981677b5bf42bec
GA9BR1 <gustavoalberttodev@gmail.com>
2024-05-22 08:56:55 -0300

    initial commit

---
+++ lib/sports/manager.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "manager/version"
+
+module Sports
+  module Manager
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/sports/manager/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Sports
+  module Manager
+    VERSION = "0.1.0"
+  end
+end
