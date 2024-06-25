4e278eb4431a69128f92c261a7db0bf905b9af52
GA9BR1 <gustavoalberttodev@gmail.com>
2024-06-25 14:30:15 -0300

    Add more match builder tests

--- spec/match_builder_spec.rb
+++ spec/match_builder_spec.rb
@@ -450,4 +450,217 @@ RSpec.describe SportsManager::MatchBuilder do
       end
     end
   end
+  describe '#extract_participant_ids' do
+    context 'when matches has already generated matches structure' do
+      context 'single matches' do
+        it 'returns a list of participant ids' do
+          matches = [
+            { id: 1, participants: [1, 2] },
+            { id: 2, participants: [3, 4] }
+          ]
+
+          participant_ids = described_class.new(category: :mixed_single, matches: matches, teams: []).send(:extract_participant_ids)
+
+          expect(participant_ids).to match_array [[1, 2], [3, 4]]
+        end
+      end
+
+      context 'double matches' do
+        it 'returns a list of participant ids' do
+          matches = [
+            { id: 1, participants: [[1, 2], [3, 4]] }
+          ]
+
+          participant_ids = described_class.new(category: :mixed_single, matches: matches, teams: []).send(:extract_participant_ids)
+
+          expect(participant_ids).to match_array [[[1, 2], [3, 4]]]
+        end
+      end
+    end
+
+    context 'when matches has not already generated matches structure' do
+      it 'returns a list of participant ids' do
+        matches = [[1, 2], [3, 4]]
+
+        participant_ids = described_class.new(category: :mixed_single, matches: matches, teams: []).send(:extract_participant_ids)
+
+        expect(participant_ids).to match_array [[1, 2], [3, 4]]
+      end
+    end
+  end
+  describe '#matches_has_already_generated_matches_structure?' do
+    context 'when matches has already generated matches structure' do
+      it 'returns true' do
+        matches = [{ id: 1, participants: [1, 2] }]
+
+        result = described_class.new(category: :mixed_single, matches: matches, teams: []).send(:matches_has_already_generated_matches_structure?)
+
+        expect(result).to be_truthy
+      end
+    end
+
+    context 'when matches has not already generated matches structure' do
+      it 'returns false' do
+        matches = [[1, 2]]
+
+        result = described_class.new(category: :mixed_single, matches: matches, teams: []).send(:matches_has_already_generated_matches_structure?)
+
+        expect(result).to be_falsey
+      end
+    end
+  end
+  describe '#build_already_generated_matches' do
+    context 'single matches' do
+      it 'returns a list of matches' do
+        matches = [
+          { id: 1, participants: [1, 2] },
+          { id: 2, participants: [3, 4] }
+        ]
+        participant1 = SportsManager::Participant.new(id: 1, name: 'João')
+        participant2 = SportsManager::Participant.new(id: 2, name: 'Marcelo')
+        participant3 = SportsManager::Participant.new(id: 3, name: 'José')
+        participant4 = SportsManager::Participant.new(id: 4, name: 'Pedro')
+
+        team1 = SportsManager::SingleTeam.new(participants: [participant1], category: :mixed_single)
+        team2 = SportsManager::SingleTeam.new(participants: [participant2], category: :mixed_single)
+        team3 = SportsManager::SingleTeam.new(participants: [participant3], category: :mixed_single)
+        team4 = SportsManager::SingleTeam.new(participants: [participant4], category: :mixed_single)
+
+        teams = [team1, team2, team3, team4]
+
+        matches_result = described_class.new(category: :mixed_single, matches: matches, teams: teams).send(:build_already_generated_matches, matches)
+
+        expect(matches_result).to match_array [
+          have_attributes(
+            class: SportsManager::Match,
+            id: 1,
+            team1: team1,
+            team2: team2
+          ),
+          have_attributes(
+            class: SportsManager::Match,
+            id: 2,
+            team1: team3,
+            team2: team4
+          )
+        ]
+      end
+    end
+
+    context 'single matches with byes' do
+      it 'returns a list of matches' do
+        matches = [
+          { id: 1, participants: [1, 2] },
+          { id: 2, participants: [3] }
+        ]
+        participant1 = SportsManager::Participant.new(id: 1, name: 'João')
+        participant2 = SportsManager::Participant.new(id: 2, name: 'Marcelo')
+        participant3 = SportsManager::Participant.new(id: 3, name: 'José')
+
+        team1 = SportsManager::SingleTeam.new(participants: [participant1], category: :mixed_single)
+        team2 = SportsManager::SingleTeam.new(participants: [participant2], category: :mixed_single)
+        team3 = SportsManager::SingleTeam.new(participants: [participant3], category: :mixed_single)
+
+        teams = [team1, team2, team3]
+
+        matches_result = described_class.new(category: :mixed_single, matches: matches, teams: teams).send(:build_already_generated_matches, matches)
+
+        expect(matches_result).to match_array [
+          have_attributes(
+            class: SportsManager::Match,
+            id: 1,
+            team1: team1,
+            team2: team2
+          ),
+          have_attributes(
+            class: SportsManager::ByeMatch,
+            id: 2,
+            team1: team3,
+            team2: SportsManager::NilTeam.new(category: :mixed_single)
+          )
+        ]
+      end
+    end
+
+    context 'double matches' do
+      it 'returns a list of matches' do
+        matches = [
+          { id: 1, participants: [[1, 2], [3, 4]] },
+          { id: 2, participants: [[5, 6], [7, 8]] }
+        ]
+        participant1 = SportsManager::Participant.new(id: 1, name: 'João')
+        participant2 = SportsManager::Participant.new(id: 2, name: 'Marcelo')
+        participant3 = SportsManager::Participant.new(id: 3, name: 'José')
+        participant4 = SportsManager::Participant.new(id: 4, name: 'Pedro')
+        participant5 = SportsManager::Participant.new(id: 5, name: 'Laura')
+        participant6 = SportsManager::Participant.new(id: 6, name: 'Karina')
+        participant7 = SportsManager::Participant.new(id: 7, name: 'Jéssica')
+        participant8 = SportsManager::Participant.new(id: 8, name: 'Daniela')
+
+        team1 = SportsManager::DoubleTeam.new(participants: [participant1, participant2], category: :mixed_single)
+        team2 = SportsManager::DoubleTeam.new(participants: [participant3, participant4], category: :mixed_single)
+        team3 = SportsManager::DoubleTeam.new(participants: [participant5, participant6], category: :mixed_single)
+        team4 = SportsManager::DoubleTeam.new(participants: [participant7, participant8], category: :mixed_single)
+
+        teams = [team1, team2, team3, team4]
+
+        matches_result = described_class.new(category: :mixed_single, matches: matches, teams: teams).send(:build_already_generated_matches, matches)
+
+        expect(matches_result).to match_array [
+          have_attributes(
+            class: SportsManager::Match,
+            id: 1,
+            team1: team1,
+            team2: team2
+          ),
+          have_attributes(
+            class: SportsManager::Match,
+            id: 2,
+            team1: team3,
+            team2: team4
+          ),
+        ]
+      end
+    end
+
+    context 'double matches with byes' do
+      it 'returns a list of matches' do
+        matches = [
+          { id: 1, participants: [[1, 2], [3, 4]] },
+          { id: 2, participants: [[5, 6]] }
+        ]
+        participant1 = SportsManager::Participant.new(id: 1, name: 'João')
+        participant2 = SportsManager::Participant.new(id: 2, name: 'Marcelo')
+        participant3 = SportsManager::Participant.new(id: 3, name: 'José')
+        participant4 = SportsManager::Participant.new(id: 4, name: 'Pedro')
+        participant5 = SportsManager::Participant.new(id: 5, name: 'Laura')
+        participant6 = SportsManager::Participant.new(id: 6, name: 'Karina')
+
+        team1 = SportsManager::DoubleTeam.new(participants: [participant1, participant2], category: :mixed_single)
+        team2 = SportsManager::DoubleTeam.new(participants: [participant3, participant4], category: :mixed_single)
+        team3 = SportsManager::DoubleTeam.new(participants: [participant5, participant6], category: :mixed_single)
+
+        nil_team = SportsManager::NilTeam.new(category: :mixed_single)
+
+        teams = [team1, team2, team3]
+
+        matches_result = described_class.new(category: :mixed_single, matches: matches, teams: teams).send(:build_already_generated_matches, matches)
+
+        expect(matches_result).to match_array [
+          have_attributes(
+            class: SportsManager::Match,
+            id: 1,
+            team1: team1,
+            team2: team2
+          ),
+          have_attributes(
+            class: SportsManager::ByeMatch,
+            id: 2,
+            team1: team3,
+            team2: nil_team
+          )
+        ]
+      end
+    end
+  end
 end
